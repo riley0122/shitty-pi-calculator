2f43f12b6cb04b6877c06ea1c6469124f8be97a2
LucasAmmer <lucasammerlaan123@gmail.com>
2023-10-11 20:30:17 +0200

    made the thing

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
-
+#include <cmath>
 // π/4 = Σ((-1)^(n+1) / (2n-1))
 // π over 4 = sum from {n=1} to ∞ {(-1)^(n+1)/(2n-1)}
 // That is the formula we are using
 
-int main()
+#define PI 3.14159
+
+float calculated = pow(-1, 1 + 1) / (2 * 1 - 1);
+
+int main(int argc, char *argv[])
 {
+    std::cout << calculated << std::endl;
+    return 0;
 }

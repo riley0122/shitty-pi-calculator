8376fb14bee0f4e969454f8fa1c9442c0311db7a
LucasAmmer <lucasammerlaan123@gmail.com>
2023-10-12 17:47:40 +0200

    Made it show the thng on quit

--- most.cpp
+++ most.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <cmath>
+#include <signal.h>
 // π/4 = Σ((-1)^(n+1) / (2n-1))
 // π over 4 = sum from {n=1} to ∞ {(-1)^(n+1)/(2n-1)}
 // That is the formula we are using
@@ -9,9 +10,20 @@
 
 double calculated = 0;
 
+void ending(int param)
+{
+    std::cout << std::endl
+              << std::endl
+              << "Our approximation: " << calculated * 4 << std::endl
+              << "Actual pi: " << PI << std::endl;
+    exit(0);
+}
+
 int main(int argc, char *argv[])
 {
     std::cout << "doing ∞ iterations" << std::endl;
+
+    signal(SIGINT, ending);
 
     unsigned int i = 0;
     for (;;)
@@ -24,9 +36,5 @@ int main(int argc, char *argv[])
             std::cout << "Current iteration: " << i << " Current Approximation: " << std::setprecision(ceil(i / 1000000) + 15) << calculated * 4 << "\r";
         }
     }
-
-    std::cout << std::endl
-              << "Our approximation: " << calculated * 4 << std::endl
-              << "Actual pi: " << PI << std::endl;
     return 0;
 }
